092e1531b1d74240b71595a9d14c63301862a249
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-04-02 13:29:46 +0800

    edit

--- VS/Bailian_OJ/Bailian_OJ/反正切函数的应用.c
+++ VS/Bailian_OJ/Bailian_OJ/反正切函数的应用.c
@@ -10,37 +10,33 @@
 /***********
 函数名：Check
 函数功能描述：判断当b+c=sum时，bc=sum*a+1有无正整数解
-             若有，返回1，否则，返回0
+			 若有，返回1，否则，返回0
 输入参数的类型和意义：1）int a:方程中给定的系数 2）int sum:b+c的值
 输出参数的类型和意义：1）int:方程是否有解
 ************/
-int Check(int a, int sum)
-{
-    int b;
-    int M;
-    //求得等式右边的值
-    M = sum * a + 1;
-    for (b = 1; b < sum; b++)
-    {
-        //判断b是否整除M，且对应的c是否满足方程
-        if ((M % b == 0) && (sum == M / b + b))
-        {
-            return 1;
-        }
-    }
-    return 0;
+int Check(int a, int sum) {
+	int b;
+	int M;
+	//求得等式右边的值
+	M = sum * a + 1;
+	for (b = 1; b < sum; b++) {
+		//判断b是否整除M，且对应的c是否满足方程
+		if ((M % b == 0) && (sum == M / b + b))
+		{
+			return 1;
+		}
+	}
+	return 0;
 }
-int main()
-{
-    int a, i;
-    //输入
-    scanf("%d", &a);
-    //计算
-    for (i = 2;; i++)
-    {
-        if (Check(a, i))
-            break;
-    }
-    //输出
-    printf("%d\n", i);
+int main() {
+	int a, i;
+	//输入
+	scanf("%d", &a);
+	//计算
+	for (i = 2;; i++) {
+		if (Check(a, i))
+			break;
+	}
+	//输出
+	printf("%d\n", i);
 }
--- VS/Bailian_OJ/Bailian_OJ/计算2的N次幂.c
+++ VS/Bailian_OJ/Bailian_OJ/计算2的N次幂.c
@@ -3,7 +3,7 @@
 作者：胡毅翔
 学号：PB18000290
 文件主要功能：
-	任意给定一个正整数 N(N<=100)，计算 2 的 N 次方的值。
+    任意给定一个正整数 N(N<=100)，计算 2 的 N 次方的值。
 ************/
 #include <stdio.h>
 #define MAX_LEN 200
@@ -16,54 +16,44 @@ int nHighestPos = 0;
 输入参数的类型和意义：无
 输出参数的类型和意义：无
 ************/
-void DoMultiply()
-{
+void DoMultiply(){
     int i;
-    for (i = 0; i <= nHighestPos; i++)
-    {
+    for (i = 0; i <= nHighestPos; i++){
         result[i] *= 2;
     }
     //进位处理
-    for (i = 0; i <= nHighestPos; i++)
-    {
-        if (result[i] >= 10)
-        {
+    for (i = 0; i <= nHighestPos; i++){
+        if (result[i] >= 10){
             result[i + 1]++;
             result[i] = result[i] % 10;
         }
     }
     //nHighestPos修改
-    if (result[nHighestPos + 1] != 0)
-    {
+    if (result[nHighestPos + 1] != 0){
         nHighestPos++;
     }
 }
-int main()
-{
+int main(){
     int n, i;
     //result[]数组初始化
-    for (i = 0; i < MAX_LEN; i++)
-    {
+    for (i = 0; i < MAX_LEN; i++){
         result[i] = 0;
     }
     //输入
     scanf("%d", &n);
     //判断输入是否合法
-    if (n < 0)
-    {
+    if (n < 0){
         printf("Illegal input value.\n");
         return 0;
     }
     result[0] = 1;
     //计算
-    while (n > 0)
-    {
+    while (n > 0){
         DoMultiply();
         n--;
     }
     //输出
-    for (i = nHighestPos; i >= 0; i--)
-    {
+    for (i = nHighestPos; i >= 0; i--){
         printf("%d", result[i]);
     }
     printf("\n");
