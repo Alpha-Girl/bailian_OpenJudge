f528112ff47098ef24b35d276529ec74ab0a901c
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-04-08 22:16:43 +0800

    style(反正切函数的应用.c)

--- VS/Bailian_OJ/Bailian_OJ/反正切函数的应用.c
+++ VS/Bailian_OJ/Bailian_OJ/反正切函数的应用.c
@@ -7,31 +7,39 @@
 解，要求给出 b + c 最小的解。
 ************/
 #include <stdio.h>
+#include <math.h>
 /***********
 函数名：Check
-函数功能描述：判断当b+c=sum时，bc=sum*a+1有无正整数解
+函数功能描述：判断当b+c=sum时，b*c=sum*a+1有无正整数解
 			 若有，返回1，否则，返回0
-输入参数的类型和意义：1）int a:方程中给定的系数 2）int sum:b+c的值
+输入参数的类型和意义：1）long long a:方程中给定的系数 2）long long sum:b+c的值
 输出参数的类型和意义：1）int:方程是否有解
 ************/
-int Check(int a, int sum) {
-	int b;
-	int M;
+int Check(long long a, long long sum) {
+	long long b;
+	long long M;
+	long long res;
 	//求得等式右边的值
 	M = sum * a + 1;
-	for (b = 1; b < sum; b++) {
-		//判断b是否整除M，且对应的c是否满足方程
-		if ((M % b == 0) && (sum == M / b + b))
+	printf("%lld\n",sum);
+	for (b = 1; b <= sum/2; b++) {
+		//计算等式左边的值
+		res = b * (sum - b);
+		if (M == res)
 		{
 			return 1;
+		}
+		else if(M < res)
+		{
+			break;
 		}
 	}
 	return 0;
 }
 int main() {
-	int a, i;
+	long long a, i;
 	//输入
-	scanf("%d", &a);
+	scanf("%lld", &a);
 	//计算
 	for (i = 2;; i++) {
 		if (Check(a, i))
@@ -39,4 +47,5 @@ int main() {
 	}
 	//输出
 	printf("%d\n", i);
+	return 0;
 }
